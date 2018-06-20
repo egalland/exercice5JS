1cbd2aed1db84223a32119fef0218ed16a6f14c7
egalland <emmanuel.galland117@gmail.com>
2018-06-20 18:23:46 +0200

    ajout securité

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 function calcul(){
-
 var firstNumber = document.getElementById("firstNumber").value;
 var secondNumber = document.getElementById("secondNumber").value;
-
-firstNumber = Math.floor(firstNumber);
-secondNumber = Math.floor(secondNumber);
-
-var result = firstNumber * secondNumber;
-alert(result);
+  if(/^[0-9.]+$/.test(firstNumber && secondNumber) == true){
+    firstNumber = Math.floor(firstNumber);
+    secondNumber = Math.floor(secondNumber);
+    var result = firstNumber * secondNumber;
+    alert(result);
+  }else{
+    alert('Ce n\'est pas un nombre');
+  }
 }
