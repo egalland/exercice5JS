dbf41568c642f231b33204939956a1074e1ddcae
egalland <emmanuel.galland117@gmail.com>
2018-06-21 09:21:39 +0200

    changement de sécurité

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function calcul(){
 var firstNumber = document.getElementById("firstNumber").value;
 var secondNumber = document.getElementById("secondNumber").value;
-  if(/^[0-9.]+$/.test(firstNumber && secondNumber) == true){
+  if((firstNumber % 1 !=0 ) && (secondNumber % 1 != 0)){
     firstNumber = Math.floor(firstNumber);
     secondNumber = Math.floor(secondNumber);
     var result = firstNumber * secondNumber;
     alert(result);
   }else{
-    alert('Ce n\'est pas un nombre');
+    alert('Ce n\'est pas des nombres à virgules');
   }
 }
