d0482113e49da2394fdacc3dd53dfe46a474ae7c
egalland <emmanuel.galland117@gmail.com>
2018-06-21 10:43:21 +0200

    sécurité à jour

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 // DECLARATION FONCTION CALCUL
 function calcul(){
   // DECLARATION ET RECUPERATION DES VARIABLES
-var firstNumber = document.getElementById("firstNumber").value;
-var secondNumber = document.getElementById("secondNumber").value;
-// VERIFICATION DES NOMBRES A VIRGULES
-  if((firstNumber % 1 !=0 ) && (secondNumber % 1 != 0)){
-    firstNumber = Math.floor(firstNumber);
-    secondNumber = Math.floor(secondNumber);
-    // MULTIPLICATION
-    var result = firstNumber * secondNumber;
-    // OUVERTURE D'UNE BOITE DE DIALOGUE AVEC LE RESULTAT
-    alert(result);
+  var firstNumber = document.getElementById("firstNumber").value;
+  var secondNumber = document.getElementById("secondNumber").value;
+  // VERIFICATION DES NOMBRES A VIRGULES
+  var regex = /^[0-9.]+$/;
+  if((regex.test(firstNumber) == true) && (regex.test(secondNumber) == true)){
+    if(((firstNumber % 1) == 0 ) || ((secondNumber % 1) == 0)){
+      // ECHEC
+      alert('Ce n\'est pas des nombres à virgules');
+    }else{
+      // MULTIPLICATION
+      firstNumber = Math.floor(firstNumber);
+      secondNumber = Math.floor(secondNumber);
+      var result = firstNumber * secondNumber;
+      // OUVERTURE D'UNE BOITE DE DIALOGUE AVEC LE RESULTAT
+      alert(result);
+    }
   }else{
-    // ECHEC
-    alert('Ce n\'est pas des nombres à virgules');
+    alert('Ce n\'est pas un nombre');
   }
 }
